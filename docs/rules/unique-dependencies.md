fe0336e66522413673ff8b7e208b3cf2aa7eea7e
Josh Goldberg <git@joshuakgoldberg.com>
2024-01-20 15:33:24 -0500

    feat: added unique-dependencies rule

---
+++ docs/rules/unique-dependencies.md
@@ -0,0 +1,7 @@
+# Enforce that if repository directory is specified, it matches the path to the package.json file (`package-json/unique-dependencies`)
+
+💼 This rule is enabled in the ✅ `recommended` config.
+
+💡 This rule is manually fixable by [editor suggestions](https://eslint.org/docs/developer-guide/working-with-rules#providing-suggestions).
+
+<!-- end auto-generated rule header -->
---
+++ src/rules/unique-dependencies.ts
@@ -0,0 +1,109 @@
+import type { AST as JsonAST } from "jsonc-eslint-parser";
+
+import * as ESTree from "estree";
+
+import { createRule } from "../createRule.js";
+import { isJSONStringLiteral, isNotNullish } from "../utils/predicates.js";
+
+const dependencyPropertyNames = new Set([
+	"bundleDependencies",
+	"bundledDependencies",
+	"dependencies",
+	"devDependencies",
+	"optionalDependencies",
+	"peerDependencies",
+	"overrides",
+]);
+
+export default createRule({
+	create(context) {
+		function check(
+			elements: (JsonAST.JSONNode | null)[],
+			getNodeToRemove: (element: JsonAST.JSONNode) => ESTree.Node,
+		) {
+			const seen = new Set();
+
+			for (const element of elements
+				.filter(isNotNullish)
+				.filter(isJSONStringLiteral)
+				.reverse()) {
+				if (seen.has(element.value)) {
+					report(element);
+				} else {
+					seen.add(element.value);
+				}
+			}
+
+			function report(node: JsonAST.JSONNode) {
+				context.report({
+					messageId: "overridden",
+					node: node as unknown as ESTree.Node,
+					suggest: [
+						{
+							fix(fixer) {
+								const removal = getNodeToRemove(node);
+								return [
+									fixer.remove(removal),
+									fixer.remove(
+										// A listing that's overridden can't be last,
+										// so we're guaranteed there's a comma after.
+										// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+										context.sourceCode.getTokenAfter(
+											removal,
+										)!,
+									),
+								];
+							},
+							messageId: "remove",
+						},
+					],
+				});
+			}
+		}
+
+		return {
+			"Program > JSONExpressionStatement > JSONObjectExpression > JSONProperty[key.type=JSONLiteral]"(
+				node: JsonAST.JSONProperty & {
+					key: JsonAST.JSONStringLiteral;
+				},
+			) {
+				if (!dependencyPropertyNames.has(node.key.value)) {
+					return;
+				}
+
+				switch (node.value.type) {
+					case "JSONArrayExpression":
+						check(
+							node.value.elements,
+							(element) => element as unknown as ESTree.Node,
+						);
+						break;
+					case "JSONObjectExpression":
+						check(
+							node.value.properties.map(
+								(property) => property.key,
+							),
+							(property) =>
+								property.parent as unknown as ESTree.Node,
+						);
+						break;
+				}
+			},
+		};
+	},
+
+	meta: {
+		docs: {
+			category: "Best Practices",
+			description:
+				"Enforce that if repository directory is specified, it matches the path to the package.json file",
+			recommended: true,
+		},
+		hasSuggestions: true,
+		messages: {
+			overridden:
+				"Package name is overridden by a duplicate listing later on.",
+			remove: "Remove this redundant dependency listing.",
+		},
+	},
+});
