830ddccbfdc3a2e2a06859ff5d9a6683063755ab
Stephen Zhou <hi@hyoban.cc>
2024-02-20 23:51:19 +0800

    feat: support flat config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import parserJsonc from "jsonc-eslint-parser";
+import { createRequire } from "node:module";
+
 import { rule as orderProperties } from "./rules/order-properties.js";
 import { rule as preferRepositoryShorthand } from "./rules/prefer-repository-shorthand.js";
 import { rule as sortCollections } from "./rules/sort-collections.js";
@@ -7,6 +10,12 @@ import { rule as validName } from "./rules/valid-name.js";
 import { rule as validPackageDef } from "./rules/valid-package-def.js";
 import { rule as validRepositoryDirectory } from "./rules/valid-repository-directory.js";
 import { rule as validVersion } from "./rules/valid-version.js";
+
+const require = createRequire(import.meta.url);
+const { name, version } = require("../package.json") as {
+	name: string;
+	version: string;
+};
 
 export const rules = {
 	"order-properties": orderProperties,
@@ -20,12 +29,53 @@ export const rules = {
 	"valid-version": validVersion,
 };
 
+const recommendedRuleSettings = Object.fromEntries(
+	Object.entries(rules)
+		.filter(([, rule]) => rule.meta.docs?.recommended)
+		.map(([name]) => ["package-json/" + name, "error" as const]),
+);
+
+interface Configs {
+	recommended: {
+		rules: typeof recommendedRuleSettings;
+	};
+	"recommended-flat": {
+		files: string[];
+		languageOptions: {
+			parser: typeof parserJsonc;
+		};
+		plugins: {
+			"package-json": typeof plugin;
+		};
+		rules: typeof recommendedRuleSettings;
+	};
+}
+
+const plugin = {
+	configs: {} as Configs,
+	meta: {
+		name,
+		version,
+	},
+	rules,
+};
+
 export const configs = {
 	recommended: {
-		rules: Object.fromEntries(
-			Object.entries(rules)
-				.filter(([, rule]) => rule.meta.docs?.recommended)
-				.map(([name]) => ["package-json/" + name, "error" as const]),
-		),
+		rules: recommendedRuleSettings,
+	},
+	["recommended-flat"]: {
+		files: ["**/package.json"],
+		languageOptions: {
+			parser: parserJsonc,
+		},
+		plugins: {
+			"package-json": plugin,
+		},
+		rules: recommendedRuleSettings,
 	},
 };
+
+Object.assign(plugin.configs, configs);
+
+export default plugin;
