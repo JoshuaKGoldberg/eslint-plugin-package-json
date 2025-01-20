70b1881a8b77cd4e35d6fa4090e1b1fe46ba908a
chouchouji <1305974212@qq.com>
2025-01-20 23:27:52 +0800

    refactor: optimize code

--- src/rules/no-empty-fields.ts
+++ src/rules/no-empty-fields.ts
@@ -1,74 +1,57 @@
-import { createRule } from "../createRule";
 import type { AST as JsonAST } from "jsonc-eslint-parser";
 
 import * as ESTree from "estree";
 
+import { createRule } from "../createRule";
+
 export const rule = createRule({
 	create(context) {
+		const report = (
+			node: JsonAST.JSONProperty & { key: JsonAST.JSONStringLiteral },
+		) => {
+			context.report({
+				data: { field: node.key.value },
+				messageId: "emptyFields",
+				node: node as unknown as ESTree.Node,
+				suggest: [
+					{
+						*fix(fixer) {
+							yield fixer.remove(node as unknown as ESTree.Node);
+
+							const tokenFromCurrentLine =
+								context.sourceCode.getTokenAfter(
+									node as unknown as ESTree.Node,
+								);
+							const tokenFromPreviousLine =
+								context.sourceCode.getTokenBefore(
+									node as unknown as ESTree.Node,
+								);
+
+							if (
+								tokenFromPreviousLine?.value === "," &&
+								tokenFromCurrentLine?.value !== ","
+							) {
+								yield fixer.remove(tokenFromPreviousLine);
+							}
+
+							if (tokenFromCurrentLine?.value === ",") {
+								yield fixer.remove(tokenFromCurrentLine);
+							}
+						},
+						messageId: "remove",
+					},
+				],
+			});
+		};
 		return {
-			"Program > JSONExpressionStatement > JSONObjectExpression > JSONProperty[key.type=JSONLiteral]"(
-				node: JsonAST.JSONProperty & {
-					key: JsonAST.JSONStringLiteral;
-				},
-			) {
-				if (
-					!["JSONArrayExpression", "JSONObjectExpression"].includes(
-						node.value.type,
-					)
-				) {
-					return;
+			JSONArrayExpression(node: JsonAST.JSONArrayExpression) {
+				if (!node.elements.length) {
+					report(node.parent as any);
 				}
-
-				if (
-					(node.value.type === "JSONArrayExpression" &&
-						!node.value.elements.length) ||
-					(node.value.type === "JSONObjectExpression" &&
-						!node.value.properties.length)
-				) {
-					context.report({
-						data: { field: node.key.value },
-						messageId: "emptyFields",
-						node: node as unknown as ESTree.Node,
-						suggest: [
-							{
-								*fix(fixer) {
-									const keys = Object.keys(
-										JSON.parse(context.sourceCode.text),
-									);
-									const index = keys.findIndex(
-										(key) => key === node.key.value,
-									);
-									const tokenFromPreviousLine =
-										context.sourceCode.getTokenAfter(
-											node as unknown as ESTree.Node,
-										);
-									if (
-										tokenFromPreviousLine?.value === "," &&
-										index === keys.length - 1
-									) {
-										yield fixer.remove(
-											tokenFromPreviousLine,
-										);
-									}
-
-									const tokenFromCurrentLine =
-										context.sourceCode.getTokenAfter(
-											node as unknown as ESTree.Node,
-										);
-									if (tokenFromCurrentLine?.value === ",") {
-										yield fixer.remove(
-											tokenFromCurrentLine,
-										);
-									}
-
-									yield fixer.remove(
-										node as unknown as ESTree.Node,
-									);
-								},
-								messageId: "remove",
-							},
-						],
-					});
+			},
+			JSONObjectExpression(node: JsonAST.JSONObjectExpression) {
+				if (!node.properties.length) {
+					report(node.parent as any);
 				}
 			},
 		};
@@ -79,13 +62,13 @@ export const rule = createRule({
 			description: "Remove empty fields",
 			recommended: true,
 		},
+		fixable: "code",
 		hasSuggestions: true,
 		messages: {
 			emptyFields: 'Should remove empty "{{field}}"',
 			remove: "Remove this empty field.",
 		},
 		schema: [],
-		fixable: "code",
 		type: "suggestion",
 	},
 });
