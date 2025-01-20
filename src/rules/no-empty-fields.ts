9608e37947b22caf087ed446234cbf821914f488
chouchouji <1305974212@qq.com>
2025-01-21 07:16:20 +0800

    refactor: optimize code

--- src/rules/no-empty-fields.ts
+++ src/rules/no-empty-fields.ts
@@ -2,52 +2,56 @@ import type { AST as JsonAST } from "jsonc-eslint-parser";
 
 import * as ESTree from "estree";
 
-import { createRule } from "../createRule";
+import { createRule, PackageJsonRuleContext } from "../createRule";
+
+const report = (
+	context: PackageJsonRuleContext,
+	node: JsonAST.JSONProperty,
+) => {
+	context.report({
+		data: { field: (node.key as JsonAST.JSONStringLiteral).value },
+		messageId: "emptyFields",
+		node: node as unknown as ESTree.Node,
+		suggest: [
+			{
+				*fix(fixer) {
+					yield fixer.remove(node as unknown as ESTree.Node);
+
+					const tokenFromCurrentLine =
+						context.sourceCode.getTokenAfter(
+							node as unknown as ESTree.Node,
+						);
+					const tokenFromPreviousLine =
+						context.sourceCode.getTokenBefore(
+							node as unknown as ESTree.Node,
+						);
+
+					if (
+						tokenFromPreviousLine?.value === "," &&
+						tokenFromCurrentLine?.value !== ","
+					) {
+						yield fixer.remove(tokenFromPreviousLine);
+					}
+
+					if (tokenFromCurrentLine?.value === ",") {
+						yield fixer.remove(tokenFromCurrentLine);
+					}
+				},
+				messageId: "remove",
+			},
+		],
+	});
+};
 
 export const rule = createRule({
 	create(context) {
-		const report = (node: JsonAST.JSONProperty) => {
-			context.report({
-				data: { field: (node.key as JsonAST.JSONStringLiteral).value },
-				messageId: "emptyFields",
-				node: node as unknown as ESTree.Node,
-				suggest: [
-					{
-						*fix(fixer) {
-							yield fixer.remove(node as unknown as ESTree.Node);
-
-							const tokenFromCurrentLine =
-								context.sourceCode.getTokenAfter(
-									node as unknown as ESTree.Node,
-								);
-							const tokenFromPreviousLine =
-								context.sourceCode.getTokenBefore(
-									node as unknown as ESTree.Node,
-								);
-
-							if (
-								tokenFromPreviousLine?.value === "," &&
-								tokenFromCurrentLine?.value !== ","
-							) {
-								yield fixer.remove(tokenFromPreviousLine);
-							}
-
-							if (tokenFromCurrentLine?.value === ",") {
-								yield fixer.remove(tokenFromCurrentLine);
-							}
-						},
-						messageId: "remove",
-					},
-				],
-			});
-		};
 		return {
 			JSONArrayExpression(node: JsonAST.JSONArrayExpression) {
 				if (
 					!node.elements.length &&
 					node.parent.type === "JSONProperty"
 				) {
-					report(node.parent);
+					report(context, node.parent);
 				}
 			},
 			JSONObjectExpression(node: JsonAST.JSONObjectExpression) {
@@ -55,7 +59,7 @@ export const rule = createRule({
 					!node.properties.length &&
 					node.parent.type === "JSONProperty"
 				) {
-					report(node.parent);
+					report(context, node.parent);
 				}
 			},
 		};
