5a9ffb773913c238ca5b058ca76f8f49a4f3bff2
chouchouji <1305974212@qq.com>
2025-02-10 22:09:31 +0800

    feat(require-files): add require-files rule

--- src/rules/require-properties.ts
+++ src/rules/require-properties.ts
@@ -6,6 +6,7 @@ import { createRequirePropertyRule } from "../utils/createRequirePropertyRule.js
 // in the format [propertyName, isRecommended]
 const properties = [
 	["author", false],
+	["files", false],
 	["keywords", false],
 	["name", true],
 	["version", true],
