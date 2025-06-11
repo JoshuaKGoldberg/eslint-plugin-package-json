f4522bda534154f25eb30785e9ee25c67c7d2917
Josh Goldberg <git@joshuakgoldberg.com>
2025-06-11 07:48:33 -0400

    pnpm format --write

--- src/rules/valid-license.ts
+++ src/rules/valid-license.ts
@@ -8,79 +8,81 @@ export type AllowedValues = string | string[] | undefined;
 export type Options = [AllowedValues];
 
 export function generateReportData(allowed: string[]) {
-    return {
-        allowed: allowed.map((v) => `'${v}'`).join(","),
-        multiple: allowed.length > 1 ? "one of" : "",
-    };
+	return {
+		allowed: allowed.map((v) => `'${v}'`).join(","),
+		multiple: allowed.length > 1 ? "one of" : "",
+	};
 }
 
 function forceToArray<T>(item: T | T[]): T[] {
-    return Array.isArray(item) ? item : [item];
+	return Array.isArray(item) ? item : [item];
 }
 
 export const rule = createRule<Options>({
-    create(context) {
-        const ruleOptions = context.options[0];
+	create(context) {
+		const ruleOptions = context.options[0];
 
-        const allowedValues = ruleOptions === undefined ?
-            undefined :
-            forceToArray(ruleOptions);
+		const allowedValues =
+			ruleOptions === undefined ? undefined : forceToArray(ruleOptions);
 
-        return {
-            "Program > JSONExpressionStatement > JSONObjectExpression > JSONProperty[key.value=license]"(
-                node: JsonAST.JSONProperty,
-            ) {
-                if (
-                    node.value.type !== "JSONLiteral" ||
-                    typeof node.value.value !== "string"
-                ) {
-                    context.report({
-                        messageId: "nonString",
-                        node: node.value as unknown as ESTree.Node,
-                    });
-                    return;
-                }
+		return {
+			"Program > JSONExpressionStatement > JSONObjectExpression > JSONProperty[key.value=license]"(
+				node: JsonAST.JSONProperty,
+			) {
+				if (
+					node.value.type !== "JSONLiteral" ||
+					typeof node.value.value !== "string"
+				) {
+					context.report({
+						messageId: "nonString",
+						node: node.value as unknown as ESTree.Node,
+					});
+					return;
+				}
 
-                if (allowedValues !== undefined && !allowedValues.includes(node.value.value)) {
-                    context.report({
-                        data: generateReportData(allowedValues),
-                        loc: node.loc,
-                        messageId: "invalidValue",
-                    });
-                }
-            },
-        };
-    },
+				if (
+					allowedValues !== undefined &&
+					!allowedValues.includes(node.value.value)
+				) {
+					context.report({
+						data: generateReportData(allowedValues),
+						loc: node.loc,
+						messageId: "invalidValue",
+					});
+				}
+			},
+		};
+	},
 
-    meta: {
-        docs: {
-            category: "Best Practices",
-            description:
-                "Enforce the 'license' field to be a specific value/values",
-            recommended: false,
-        },
-        messages: {
-            invalidValue: '"license" must be{{multiple}} {{allowed}}"',
-            nonString: '"license" must be a string"',
-        },
-        schema: {
-            items: [
-                {
-                    oneOf: [
-                        {
-                            items: {
-                                type: "string",
-                            },
-                            type: "array",
-                        },
-                        {
-                            type: "string",
-                        },
-                    ],
-                },
-            ],
-            type: "array",
-        },
-        type: "problem",
-    },
+	meta: {
+		docs: {
+			category: "Best Practices",
+			description:
+				"Enforce the 'license' field to be a specific value/values",
+			recommended: false,
+		},
+		messages: {
+			invalidValue: '"license" must be{{multiple}} {{allowed}}"',
+			nonString: '"license" must be a string"',
+		},
+		schema: {
+			items: [
+				{
+					oneOf: [
+						{
+							items: {
+								type: "string",
+							},
+							type: "array",
+						},
+						{
+							type: "string",
+						},
+					],
+				},
+			],
+			type: "array",
+		},
+		type: "problem",
+	},
 });
--- src/tests/rules/valid-license.test.ts
+++ src/tests/rules/valid-license.test.ts
@@ -2,81 +2,81 @@ import { generateReportData, rule } from "../../rules/valid-license.js";
 import { ruleTester } from "./ruleTester.js";
 
 ruleTester.run("valid-license", rule, {
-    invalid: [
-        {
-            code: JSON.stringify({
-                license: "CC BY-SA",
-                name: "some-test-package",
-            }),
-            errors: [
-                {
-                    data: generateReportData(["GPL"]),
-                    messageId: "invalidValue",
-                },
-            ],
-            name: "Invalid with single valid value",
-            options: ["GPL"],
-        },
-        {
-            code: JSON.stringify({
-                license: "Apache",
-                name: "some-test-package",
-            }),
-            errors: [
-                {
-                    data: generateReportData(["MIT", "GPL"]),
-                    messageId: "invalidValue",
-                },
-            ],
-            name: "Invalid with multiple valid values",
-            options: [["MIT", "GPL"]],
-        },
-        {
-            code: JSON.stringify({
-                license: 1234,
-                name: "some-test-package",
-            }),
-            errors: [
-                {
-                    data: generateReportData(["GPL"]),
-                    messageId: "nonString",
-                },
-            ],
-            name: "Invalid property type",
-            options: ["GPL"],
-        },
-    ],
-    valid: [
-        {
-            code: JSON.stringify({
-                license: "Apache",
-                name: "some-test-package",
-            }),
-            name: "Valid value with no configuration",
-            options: [],
-        },
-        {
-            code: JSON.stringify({
-                license: "Apache",
-                name: "some-test-package",
-            }),
-            name: "Valid value from single valid value",
-            options: ["Apache"],
-        },
-        {
-            code: JSON.stringify({
-                license: "GPL",
-                name: "some-test-package",
-            }),
-            name: "Valid value from multiple valid values",
-            options: [["MIT", "GPL"]],
-        },
-        {
-            code: JSON.stringify({
-                name: "some-test-package",
-            }),
-            name: "Missing property",
-            options: [["MIT", "GPL"]],
-        },
-    ],
+	invalid: [
+		{
+			code: JSON.stringify({
+				license: "CC BY-SA",
+				name: "some-test-package",
+			}),
+			errors: [
+				{
+					data: generateReportData(["GPL"]),
+					messageId: "invalidValue",
+				},
+			],
+			name: "Invalid with single valid value",
+			options: ["GPL"],
+		},
+		{
+			code: JSON.stringify({
+				license: "Apache",
+				name: "some-test-package",
+			}),
+			errors: [
+				{
+					data: generateReportData(["MIT", "GPL"]),
+					messageId: "invalidValue",
+				},
+			],
+			name: "Invalid with multiple valid values",
+			options: [["MIT", "GPL"]],
+		},
+		{
+			code: JSON.stringify({
+				license: 1234,
+				name: "some-test-package",
+			}),
+			errors: [
+				{
+					data: generateReportData(["GPL"]),
+					messageId: "nonString",
+				},
+			],
+			name: "Invalid property type",
+			options: ["GPL"],
+		},
+	],
+	valid: [
+		{
+			code: JSON.stringify({
+				license: "Apache",
+				name: "some-test-package",
+			}),
+			name: "Valid value with no configuration",
+			options: [],
+		},
+		{
+			code: JSON.stringify({
+				license: "Apache",
+				name: "some-test-package",
+			}),
+			name: "Valid value from single valid value",
+			options: ["Apache"],
+		},
+		{
+			code: JSON.stringify({
+				license: "GPL",
+				name: "some-test-package",
+			}),
+			name: "Valid value from multiple valid values",
+			options: [["MIT", "GPL"]],
+		},
+		{
+			code: JSON.stringify({
+				name: "some-test-package",
+			}),
+			name: "Missing property",
+			options: [["MIT", "GPL"]],
+		},
+	],
 });
