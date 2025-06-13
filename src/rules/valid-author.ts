b8e8b40065a0aa0bfa9e38697ab5fe3241b7d6d7
Azat S. <to@azat.io>
2025-06-13 15:26:56 +0300

    feat: add `valid-author` rule (#1079)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #840
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Added new rule: `valid-author`.
    
    The rule leverages the `package-json-validator` library for validation
    and provides detailed error messages when the specification is violated.
    
    Tests have been added covering various cases of correct and incorrect
    usage of the `author` field, along with documentation that includes
    examples.
    
    The `package-json-validator` library has also been updated. And we had
    to tweak the code for the `valid-package-definition` rule a bit.

---
+++ src/rules/valid-author.ts
@@ -0,0 +1,53 @@
+import type { AST as JsonAST } from "jsonc-eslint-parser";
+
+import * as ESTree from "estree";
+import { validateAuthor } from "package-json-validator";
+
+import { createRule } from "../createRule.js";
+
+export const rule = createRule({
+	create(context) {
+		return {
+			"Program > JSONExpressionStatement > JSONObjectExpression > JSONProperty[key.value=author]"(
+				node: JsonAST.JSONProperty,
+			) {
+				const authorValue: unknown = JSON.parse(
+					context.sourceCode.getText(
+						node.value as unknown as ESTree.Node,
+					),
+				);
+
+				const errors = validateAuthor(authorValue);
+
+				if (errors.length > 0) {
+					const complaints =
+						errors.length === 1
+							? errors[0]
+							: `\n - ${errors.join("\n - ")}`;
+
+					context.report({
+						data: {
+							complaints,
+						},
+						messageId: "invalid",
+						node: node.value as unknown as ESTree.Node,
+					});
+				}
+			},
+		};
+	},
+
+	meta: {
+		docs: {
+			category: "Best Practices",
+			description:
+				"Enforce that the author field is a valid npm author specification",
+			recommended: true,
+		},
+		messages: {
+			invalid: "Invalid author: {{ complaints }}",
+		},
+		schema: [],
+		type: "problem",
+	},
+});
---
+++ src/tests/rules/valid-author.test.ts
@@ -0,0 +1,196 @@
+import { rule } from "../../rules/valid-author.js";
+import { ruleTester } from "./ruleTester.js";
+
+ruleTester.run("valid-author", rule, {
+	invalid: [
+		{
+			code: `{ "author": null }`,
+			errors: [
+				{
+					message:
+						'Invalid author: Type for field "author" should be a `string` or an `object` with at least a `name` property',
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": 123 }`,
+			errors: [
+				{
+					message:
+						'Invalid author: Type for field "author" should be a `string` or an `object` with at least a `name` property',
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": true }`,
+			errors: [
+				{
+					message:
+						'Invalid author: Type for field "author" should be a `string` or an `object` with at least a `name` property',
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": [] }`,
+			errors: [
+				{
+					message:
+						'Invalid author: Type for field "author" should be a `string` or an `object` with at least a `name` property',
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "" }`,
+			errors: [
+				{
+					message: "Invalid author: author field should have name",
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "   " }`,
+			errors: [
+				{
+					message: "Invalid author: author field should have name",
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "John <invalid>" }`,
+			errors: [
+				{
+					message:
+						"Invalid author: Email not valid for author: invalid",
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "John (not-url)" }`,
+			errors: [
+				{
+					message:
+						"Invalid author: URL not valid for author: not-url",
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "<john@example.com>" }`,
+			errors: [
+				{
+					message: "Invalid author: author field should have name",
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": {} }`,
+			errors: [
+				{
+					message:
+						'Invalid author: Type for field "author" should be a `string` or an `object` with at least a `name` property',
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": { "email": "john@example.com" } }`,
+			errors: [
+				{
+					message:
+						'Invalid author: Type for field "author" should be a `string` or an `object` with at least a `name` property',
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": { "name": "" } }`,
+			errors: [
+				{
+					message: "Invalid author: author field should have name",
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": { "name": "John", "email": "invalid" } }`,
+			errors: [
+				{
+					message:
+						"Invalid author: Email not valid for author: invalid",
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": { "name": "John", "url": "invalid" } }`,
+			errors: [
+				{
+					message:
+						"Invalid author: URL not valid for author: invalid",
+				},
+			],
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "John <invalid> (invalid)" }`,
+			errors: [
+				{
+					message:
+						"Invalid author: \n - Email not valid for author: invalid\n - URL not valid for author: invalid",
+				},
+			],
+			filename: "package.json",
+		},
+	],
+
+	valid: [
+		{
+			code: `{}`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "John Doe" }`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "John <john@example.com>" }`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "John (https://example.com)" }`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": "John <john@example.com> (https://example.com)" }`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": { "name": "John" } }`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": { "name": "John", "email": "john@example.com" } }`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": { "name": "John", "url": "https://example.com" } }`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": { "name": "John", "email": "john@example.com", "url": "https://example.com" } }`,
+			filename: "package.json",
+		},
+		{
+			code: `{ "author": null }`,
+			filename: "not-a-package.json",
+		},
+	],
+});
