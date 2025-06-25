3fd9a440f2075221ec4a3dc7d18bcf1c48d8144f
michael faith <michaelfaith@users.noreply.github.com>
2025-06-25 11:07:01 -0500

    ci: run tests on all supported node versions (#1126)
    <!-- 👋 Hi, thanks for sending a PR to eslint-plugin-package-json! 🗂
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1127
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    This change updates our ci workflow to run the test stage in CI against
    node 20, 22, and 24. This ensures that all supported versions of node
    work as expected.

--- eslint.config.ts
+++ eslint.config.ts
@@ -99,10 +99,6 @@ export default tseslint.config(
 		files: ["**/*.{yml,yaml}"],
 		rules: {
 			"yml/file-extension": ["error", { extension: "yml" }],
-			"yml/sort-keys": [
-				"error",
-				{ order: { type: "asc" }, pathPattern: "^.*$" },
-			],
 			"yml/sort-sequence-values": [
 				"error",
 				{ order: { type: "asc" }, pathPattern: "^.*$" },
