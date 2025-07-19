a87369d4009c546b8b9a4ab8f17e75263b32e9ad
michael faith <michaelfaith@users.noreply.github.com>
2025-07-19 16:47:34 -0500

    feat(valid-config): add new rule for validating `config`
    This change adds a new `valid-config` rule that uses `validateConfig` from package-json-validator to identify errors.  It's included in the `recommended` config.

---
+++ src/tests/rules/valid-config.test.ts
@@ -0,0 +1,69 @@
+import { rule } from "../../rules/valid-config.js";
+import { ruleTester } from "./ruleTester.js";
+
+ruleTester.run("valid-config", rule, {
+	invalid: [
+		{
+			code: `{
+	"config": null
+}
+`,
+			errors: [
+				{
+					data: {
+						errors: "the field is `null`, but should be an `object`",
+					},
+					messageId: "validationError",
+				},
+			],
+		},
+		{
+			code: `{
+	"config": 123
+}
+`,
+			errors: [
+				{
+					data: {
+						errors: "the type should be `object`, not `number`",
+					},
+					messageId: "validationError",
+				},
+			],
+		},
+		{
+			code: `{
+	"config": "./script.js"
+}
+`,
+			errors: [
+				{
+					data: {
+						errors: "the type should be `object`, not `string`",
+					},
+					messageId: "validationError",
+				},
+			],
+		},
+		{
+			code: `{
+	"config": []
+}
+`,
+			errors: [
+				{
+					data: {
+						errors: "the type should be `object`, not `array`",
+					},
+					messageId: "validationError",
+				},
+			],
+		},
+	],
+	valid: [
+		"{}",
+		`{ "config": { "silver-mt-zion": "node ./silver-mt-zion.js" } }`,
+		`{ "config": { "silver-mt-zion": "node ./silver-mt-zion.js", "nin": "node ./nin.js" } }`,
+		`{ "config": { "silver-mt-zion": {"leadSinger": "Efrim Manuel Menuck"}, "nin": "node ./nin.js" } }`,
+	],
+});
