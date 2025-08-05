944ced332790d13f670361dfad8c0564b1c2f92a
michael faith <michaelfaith@users.noreply.github.com>
2025-08-05 17:21:46 -0500

    add tests back after merge conflict resolution

--- src/tests/rules/require-properties.test.ts
+++ src/tests/rules/require-properties.test.ts
@@ -1,3 +1,5 @@
+import type { PackageJsonPluginSettings } from "../../createRule.js";
+
 import { rules } from "../../rules/require-properties.js";
 import { ruleTester } from "./ruleTester.js";
 
@@ -49,12 +51,321 @@ ruleNames.forEach((ruleName) => {
 					},
 				],
 			},
+			{
+				code: `{
+          "private": true
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: false }],
+			},
+			{
+				code: `{
+          "private": false
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: true }],
+			},
+			{
+				code: `{
+          "private": "true"
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: true }],
+			},
+			{
+				code: `{}`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: true }],
+			},
+			{
+				code: `{
+          "private": false
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+			},
+			{
+				code: `{
+          "private": true
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				settings: {
+					packageJson: {
+						enforceForPrivate: true,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": true
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: false }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: true,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": false
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: true }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: true,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{}`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: true }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: true,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": true
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: false }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: false,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": false
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				settings: {
+					packageJson: {
+						enforceForPrivate: false,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": false
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: false }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: false,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": false
+          }`,
+				errors: [
+					{
+						data: { property: propertyName },
+						line: 1,
+						messageId: "missing",
+					},
+				],
+				options: [{ ignorePrivate: true }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: false,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
 		],
 		valid: [
 			`{ "foo": "./index.js", "${propertyName}": "Sophie Trudeau" }`,
 			`{ "${propertyName}": "Jessica Moss" }`,
 			`{ "${propertyName}": 123 }`,
 			`{ "${propertyName}": { "name": "Jessica Moss" } }`,
+			{
+				code: `{
+          "private": true,
+          "${propertyName}": "Jessica Moss"
+          }`,
+			},
+			{
+				code: `{
+          "private": true
+          }`,
+				options: [{ ignorePrivate: true }],
+			},
+			{
+				code: `{
+          "private": true,
+          "${propertyName}": "Jessica Moss"
+          }`,
+				options: [{ ignorePrivate: false }],
+			},
+			{
+				code: `{
+          "private": false,
+          "${propertyName}": "Jessica Moss"
+          }`,
+				options: [{ ignorePrivate: true }],
+			},
+			{
+				code: `{
+          "private": false,
+          "${propertyName}": "Jessica Moss"
+          }`,
+				options: [{ ignorePrivate: false }],
+			},
+			{
+				code: `{
+          "private": "true",
+          "${propertyName}": "Jessica Moss"
+          }`,
+				options: [{ ignorePrivate: true }],
+			},
+			{
+				code: `{
+          "private": true,
+          "${propertyName}": "Jessica Moss"
+          }`,
+				settings: {
+					packageJson: {
+						enforceForPrivate: true,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": true
+          }`,
+				options: [{ ignorePrivate: true }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: true,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": true,
+          "${propertyName}": "Jessica Moss"
+          }`,
+				options: [{ ignorePrivate: false }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: true,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": true
+          }`,
+				settings: {
+					packageJson: {
+						enforceForPrivate: false,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": true
+          }`,
+				options: [{ ignorePrivate: true }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: false,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
+			{
+				code: `{
+          "private": true,
+          "${propertyName}": "Jessica Moss"
+          }`,
+				options: [{ ignorePrivate: false }],
+				settings: {
+					packageJson: {
+						enforceForPrivate: false,
+					} satisfies PackageJsonPluginSettings,
+				},
+			},
 		],
 	});
 });
