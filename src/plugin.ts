20609105a340d4aadb2bd351034e2853bb7e084e
michael faith <michaelfaith@users.noreply.github.com>
2025-10-25 15:28:27 -0500

    disable no-deprecated rule

--- src/plugin.ts
+++ src/plugin.ts
@@ -79,6 +79,7 @@ const recommendedRules = {
 const stylisticRules = {
 	...Object.fromEntries(
 		Object.entries(rules)
+			// eslint-disable-next-line @typescript-eslint/no-deprecated
 			.filter(([, rule]) => rule.meta.docs?.category === "Stylistic")
 			.map(([name]) => ["package-json/" + name, "error" as const]),
 	),
