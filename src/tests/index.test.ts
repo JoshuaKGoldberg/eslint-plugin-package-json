3d01cce99a1718a7485669c7b8cf6143a255b094
michael faith <michaelfaith@users.noreply.github.com>
2025-11-03 12:35:03 -0600

    feat: add new stylistic config (#1342)
    <!-- 👋 Hi, thanks for sending a PR to eslint-plugin-package-json! 🗂
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1341
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    This change adds a new `stylistic` config, which will include rules that
    are more opinionated about the style of a package.json. The first rule
    for this new config is the recently added `exports-subpaths-style`.
    
    ---------
    
    Co-authored-by: Josh Goldberg ✨ <git@joshuakgoldberg.com>

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -6,9 +6,30 @@ import { describe, expect, it } from "vitest";
 import plugin from "../index.ts";
 
 describe("configs", () => {
-	it("configs should work properly", async () => {
+	it("recommended config works properly", async () => {
 		const eslint = new ESLint({
 			baseConfig: plugin.configs.recommended as Linter.Config,
+			fix: true,
+			overrideConfigFile: true,
+		});
+		const code = await readFile(
+			resolve(import.meta.filename, "../../../package.json"),
+			"utf8",
+		);
+		const result = await eslint.lintText(code, {
+			filePath: "package.json",
+		});
+		expect(
+			result[0].messages.map((message) => ({
+				message: message.message,
+				ruleId: message.ruleId,
+			})),
+		).toEqual([]);
+	});
+
+	it("stylistic config works properly", async () => {
+		const eslint = new ESLint({
+			baseConfig: plugin.configs.stylistic as Linter.Config,
 			fix: true,
 			overrideConfigFile: true,
 		});
