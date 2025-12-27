6da77901996e59f57f3ddddacae3569c92cc4b05
michael faith <michaelfaith@users.noreply.github.com>
2025-12-27 15:15:33 -0600

    fix(repository-shorthand): support repo provider prefix
    This change updates all fixers to support different repo providers, and the providers prefix in general.  Now if you have repo defined in object syntax with either bitbucket, gitlab, github, or gist urls, the fixer will construct a shorthand string using the corresponding provider prefix.  On the other side, if you have repo defined as a shorthand and have configured to prefer object, it'll use the correct url based on the provider prefix, if specified (assume github if not).

--- src/tests/rules/repository-shorthand.test.ts
+++ src/tests/rules/repository-shorthand.test.ts
@@ -35,12 +35,12 @@ ruleTester.run("repository-shorthand", rule, {
 		},
 		{
 			code: `{
-				"repository": "JoshuaKGoldberg/create-typescript-app"
+				"repository": "github:JoshuaKGoldberg/create-typescript-app"
 			}`,
 			errors: [
 				{
 					column: 19,
-					endColumn: 58,
+					endColumn: 65,
 					messageId: "preferObject",
 				},
 			],
@@ -60,15 +60,14 @@ ruleTester.run("repository-shorthand", rule, {
 			}`,
 			errors: [
 				{
-					message:
-						"Prefer a shorthand locator for a GitHub repository.",
+					messageId: "preferShorthand",
 					type: "JSONObjectExpression",
 				},
 			],
 			filename: "package.json",
 			options: [{ form: "shorthand" }],
 			output: `{
-				"repository": "JoshuaKGoldberg/create-typescript-app"
+				"repository": "github:JoshuaKGoldberg/create-typescript-app"
 			}`,
 		},
 		{
@@ -80,15 +79,14 @@ ruleTester.run("repository-shorthand", rule, {
 			}`,
 			errors: [
 				{
-					message:
-						"Prefer a shorthand locator for a GitHub repository.",
+					messageId: "preferShorthand",
 					type: "JSONObjectExpression",
 				},
 			],
 			filename: "package.json",
 			options: [{ form: "shorthand" }],
 			output: `{
-				"repository": "JoshuaKGoldberg/create-typescript-app"
+				"repository": "github:JoshuaKGoldberg/create-typescript-app"
 			}`,
 		},
 		{
@@ -100,15 +98,14 @@ ruleTester.run("repository-shorthand", rule, {
 			}`,
 			errors: [
 				{
-					message:
-						"Prefer a shorthand locator for a GitHub repository.",
+					messageId: "preferShorthand",
 					type: "JSONObjectExpression",
 				},
 			],
 			filename: "package.json",
 			options: [{ form: "shorthand" }],
 			output: `{
-				"repository": "JoshuaKGoldberg/create-typescript-app"
+				"repository": "github:JoshuaKGoldberg/create-typescript-app"
 			}`,
 		},
 		{
@@ -117,16 +114,72 @@ ruleTester.run("repository-shorthand", rule, {
 			}`,
 			errors: [
 				{
-					message:
-						"Prefer a shorthand locator for a GitHub repository.",
+					messageId: "preferShorthand",
 					type: "JSONLiteral",
 				},
 			],
 			filename: "package.json",
 			options: [{ form: "shorthand" }],
 			output: `{
-				"repository": "eslint/eslint"
+				"repository": "github:eslint/eslint"
 			}`,
+		},
+		{
+			code: `{
+				"repository": {
+					"type": "git",
+					"url": "https://gitlab.com/JoshuaKGoldberg/create-typescript-app"
+				}
+		}`,
+			errors: [
+				{
+					messageId: "preferShorthand",
+					type: "JSONObjectExpression",
+				},
+			],
+			filename: "package.json",
+			options: [{ form: "shorthand" }],
+			output: `{
+				"repository": "gitlab:JoshuaKGoldberg/create-typescript-app"
+		}`,
+		},
+		{
+			code: `{
+				"repository": {
+					"type": "git",
+					"url": "https://bitbucket.org/eslint/rewrite"
+				}
+		}`,
+			errors: [
+				{
+					messageId: "preferShorthand",
+					type: "JSONObjectExpression",
+				},
+			],
+			filename: "package.json",
+			options: [{ form: "shorthand" }],
+			output: `{
+				"repository": "bitbucket:eslint/rewrite"
+		}`,
+		},
+		{
+			code: `{
+				"repository": {
+					"type": "git",
+					"url": "https://gist.github.com/1234567890abcdef"
+				}
+		}`,
+			errors: [
+				{
+					messageId: "preferShorthand",
+					type: "JSONObjectExpression",
+				},
+			],
+			filename: "package.json",
+			options: [{ form: "shorthand" }],
+			output: `{
+				"repository": "gist:1234567890abcdef"
+		}`,
 		},
 	],
 	valid: [
@@ -145,6 +198,24 @@ ruleTester.run("repository-shorthand", rule, {
 			"repository": {
 				"type": "git",
 				"url": "https://github.com/JoshuaKGoldberg/create-typescript-app"
+			}
+		}`,
+		`{
+			"repository": {
+				"type": "git",
+				"url": "https://gist.github.com/JoshuaKGoldberg/1234567890abcdef"
+			}
+		}`,
+		`{
+			"repository": {
+				"type": "git",
+				"url": "https://bitbucket.org/JoshuaKGoldberg/create-typescript-app"
+			}
+		}`,
+		`{
+			"repository": {
+				"type": "git",
+				"url": "https://gitlab.com/JoshuaKGoldberg/create-typescript-app"
 			}
 		}`,
 		{
@@ -191,21 +262,29 @@ ruleTester.run("repository-shorthand", rule, {
 		},
 		{
 			code: `{
-				"repository": {
-					"type": "git",
-					"url": "https://gitlab.com/gitlab/gitlab"
-				}
-		}`,
+				"repository": "github:browserslist/browserslist"
+			}`,
 			filename: "package.json",
 			options: [{ form: "shorthand" }],
 		},
 		{
 			code: `{
-				"repository": {
-					"type": "git",
-					"url": "https://gitlab.com/a/b/c"
-				}
-		}`,
+				"repository": "gist:1234567890abcdef"
+			}`,
+			filename: "package.json",
+			options: [{ form: "shorthand" }],
+		},
+		{
+			code: `{
+				"repository": "bitbucket:browserslist/browserslist"
+			}`,
+			filename: "package.json",
+			options: [{ form: "shorthand" }],
+		},
+		{
+			code: `{
+				"repository": "gitlab:browserslist/browserslist"
+			}`,
 			filename: "package.json",
 			options: [{ form: "shorthand" }],
 		},
