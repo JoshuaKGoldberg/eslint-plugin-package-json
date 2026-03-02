53679e49f10a6ab8a7808e4eb2981a4a3ff9d34c
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2026-03-02 23:17:47 +0100

    chore: restore `prettier-plugin-sentences-per-line` (#1617)
    <!-- 👋 Hi, thanks for sending a PR to eslint-plugin-package-json! 🗂
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1501
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/michaelfaith/eslint-plugin-package-json/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/michaelfaith/eslint-plugin-package-json/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->

--- prettier.config.js
+++ prettier.config.js
@@ -7,8 +7,7 @@ const config = {
 	plugins: [
 		"prettier-plugin-curly",
 		"prettier-plugin-packagejson",
-		// To be restored once https://github.com/JoshuaKGoldberg/sentences-per-line/issues/866 is resolved
-		// "prettier-plugin-sentences-per-line",
+		"prettier-plugin-sentences-per-line",
 		"prettier-plugin-sh",
 	],
 	useTabs: true,
