e2eed273ec5ff25fc34e3d13f36c9d76020eaf61
michael faith <michaelfaith@users.noreply.github.com>
2026-04-20 17:39:31 -0500

    chore: create base site skeleton (#1723)
    <!-- 👋 Hi, thanks for sending a PR to eslint-plugin-package-json! 🗂
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1666
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/michaelfaith/eslint-plugin-package-json/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/michaelfaith/eslint-plugin-package-json/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    This change creates a new `site` dir in the repo with an Astro Starlight
    project.
    
    I'm thinking of going with the Catppuccin theme:
    https://starlight.catppuccin.com/
    
    I've also added the appropriate checks in CI for running the type check
    on the site.
    
    <img width="1415" height="688" alt="screenshot of home page"
    src="https://github.com/user-attachments/assets/331e9a5d-c45b-4343-8463-fece57c06c13"
    />

---
+++ astro.config.mjs
@@ -0,0 +1,49 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightCatppuccin from "@catppuccin/starlight";
+
+const site = "https://esling-plugin-package-json.dev";
+
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: "ESLint Plugin: Package JSON",
+			plugins: [starlightCatppuccin()],
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/michaelfaith/eslint-plugin-package-json",
+				},
+			],
+			sidebar: [
+				{
+					label: "Guides",
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: "Example Guide", slug: "guides/example" },
+					],
+				},
+				{
+					label: "Reference",
+					autogenerate: { directory: "reference" },
+				},
+			],
+		}),
+	],
+	prefetch: {
+		defaultStrategy: "hover",
+	},
+	experimental: {
+		clientPrerender: true,
+	},
+	server: {
+		port: 3000,
+	},
+	publicDir: "./site/public",
+	srcDir: "./site/src",
+	root: "./site",
+	outDir: "./dist-site",
+	site,
+});
